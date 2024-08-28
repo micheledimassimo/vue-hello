3330807600bf5e04f0d81d1bce82336d288ade82
micheledimassimo <mcldms@outlook.it>
2024-08-28 12:58:01 +0200

    hello vue

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!'
+      
+    }
+  }
+}).mount('#app')
+
+
+
+/* createApp({
+    data() {
+      return {
+        Image:'https://www.google.com/imgres?q=google&imgurl=https%3A%2F%2Fplay-lh.googleusercontent.com%2F1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA%3Dw3840-h2160-rw&imgrefurl=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdev%3Fid%3D5700313618786177705%26hl%3Dit&docid=cw7aWx8_BIOz6M&tbnid=mNvT3T4Zgwn3FM&vet=12ahUKEwjfooPYwZeIAxUqi_0HHZnpNCsQM3oECBwQAA..i&w=3840&h=2160&hcb=2&itg=1&ved=2ahUKEwjfooPYwZeIAxUqi_0HHZnpNCsQM3oECBwQAA'
+        
+      }
+    }
+  }).mount('#img') */
